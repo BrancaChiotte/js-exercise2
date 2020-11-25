ef0d29e5dfc06873e95bb6718bed897d173d7298
Branca Chiotte <brancachiotte@gmail.com>
2020-11-25 16:04:44 +0000

    not good but print ok

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ function makeData(city, country) {
 // Don’t use the ‘for’ or ‘while’ methods for it.
 // (Note: Define whatever variables you think you will need age,city and country values will be at your choice)
 
-var age = [11, 36, 28, 7, 38];
+var age = [11, 36, 28, 17, 38];
 var cities = ["Lisbon", "Porto", "Braga", "Coimbra", "Faro"];
 var country = ["Portugal"];
 
+arrInfo = [];
 namesSum.forEach(function (name, i) {
   var x = {
     name: name,
@@ -30,27 +31,27 @@ namesSum.forEach(function (name, i) {
   };
   var city = cities[Math.floor(Math.random() * cities.length)];
   var allInfo = makeData.call(x, city, country);
-  console.log(allInfo);
+  arrInfo.push(allInfo);
+
 });
+
 
 // 3 - Change the function makeData and convert it into an ES6 arrow function
 
 const makeDataEs6 = (city, country) => `Hello my name is ${name}, I am ${age} and I live in ${city}, ${country}`;
 
+
 // 4 - Add the return sentence of the function, using the filter method, to two different arrays. One for adults (age>= 18) and other for kids (age<18)
 
-
-var adults = [];
-var adults = age.filter(function (numb) {
-  return numb >= 18;
+var kids = arrInfo.filter((info) => {
+  return info.match(/[0][0-9]| 1[0-7]/)
 });
-adults;
 
-var kids = [];
-var kids = age.filter(function (numb) {
-  return numb < 18;
+var adults = arrInfo.filter((info) => {
+  console.log(info)
+  return info.match(/ 1[8-9]|[2-9][0-9]/)
 });
-kids;
+
 
 // 5 -Using the following HTML code insert those messages in the respective area. For displaying the messages you can use ul/li elements or div elements
 
